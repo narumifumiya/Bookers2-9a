bec43ff467a81bbad3ef4b52c88898cc98f35ebd
narumifumiya <124240213+narumifumiya@users.noreply.github.com>
2023-03-03 08:08:36 +0000

    [いいね機能追加]

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,15 +3,17 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
+
   has_many :books, dependent: :destroy
   has_one_attached :profile_image
+
+  has_many :favorites, dependent: :destroy
 
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
   validates :introduction, length: { maximum: 50 }
 
-  
-  
+
+
   def get_profile_image
     (profile_image.attached?) ? profile_image : 'no_image.jpg'
   end
