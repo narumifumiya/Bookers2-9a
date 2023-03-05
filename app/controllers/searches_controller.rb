6abe59411847f86e449c47905245cc9021dbe5d9
narumifumiya <124240213+narumifumiya@users.noreply.github.com>
2023-03-05 14:07:27 +0000

    [finish]view完成

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -5,6 +5,7 @@ class SearchesController < ApplicationController
     @range = params[:range]
     
     if @range == "User"
+      # user.ruとbook.rbに記述したself.looks(search,word)メソッドを使用
       @users = User.looks(params[:search], params[:word])
     else
       @books = Book.looks(params[:search], params[:word])
