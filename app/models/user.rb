ac4d049270ad2a709fbaee397f8581d75d5d31b4
narumifumiya <124240213+narumifumiya@users.noreply.github.com>
2023-03-05 07:36:40 +0000

    [微調整]

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,11 +10,15 @@ class User < ApplicationRecord
   has_many :favorites, dependent: :destroy
   
   # フォローをした、されたの関係
+  # 自分がフォローする（与フォロー）側の関係性
   has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  # 自分がフォローされる（被フォロー）側の関係性
   has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
   
   # 一覧画面で使う
+  # 与フォロー関係を通じて参照→自分がフォローしている人
   has_many :followings, through: :relationships, source: :followed
+  # 被フォロー関係を通じて参照→自分をフォローしている人
   has_many :followers, through: :reverse_of_relationships, source: :follower
 
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
