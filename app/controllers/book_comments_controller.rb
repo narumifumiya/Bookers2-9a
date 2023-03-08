24b8708f8f9bb3f9b678aebeae8b6151aedea9df
narumifumiya <124240213+narumifumiya@users.noreply.github.com>
2023-03-08 09:35:57 +0000

    [コメント非同期追加]

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,13 +1,14 @@
 class BookCommentsController < ApplicationController
 
   def create
-    book = Book.find(params[:book_id])
-    book_comment = current_user.book_comments.new(book_comment_params)
-    book_comment.book_id = book.id
-    book_comment.save
+    @book = Book.find(params[:book_id])
+    @book_comment = current_user.book_comments.new(book_comment_params)
+    @book_comment.book_id = @book.id
+    @book_comment.save
     # redirect_back fallback_location: root_path 下の記述の方が良い？
-    redirect_to request.referer
-
+    # redirect_to request.referer
+    # render :book_comments
+    
   end
 
   def destroy
@@ -17,8 +18,10 @@ class BookCommentsController < ApplicationController
     # 下記の記述でも問題ない。
     # book_comment = BookComment.find(params[:id])
     # book_comment.destroy
+    @book = Book.find(params[:book_id])
+    # render :book_comments
     # redirect_back fallback_location: root_path　下の記述の方が良い？
-    redirect_to request.referer
+    # redirect_to request.referer
   end
 
 
