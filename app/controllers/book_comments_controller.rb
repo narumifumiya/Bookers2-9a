79be729ac9ff7c505ca99fe8ac7ad269ef954904
narumifumiya <124240213+narumifumiya@users.noreply.github.com>
2023-03-08 12:14:40 +0000

    [コメント非同期化　修正]

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,24 +1,23 @@
 class BookCommentsController < ApplicationController
 
   def create
-    @book = Book.find(params[:book_id])
+    book = Book.find(params[:book_id])
     @book_comment = current_user.book_comments.new(book_comment_params)
-    @book_comment.book_id = @book.id
+    @book_comment.book_id = book.id
     @book_comment.save
     # redirect_back fallback_location: root_path 下の記述の方が良い？
     # redirect_to request.referer
     # render :book_comments
-    
+
   end
 
   def destroy
     # 削除するコメントを見つける
-    # 　　　　　　　　　comment.id↓　　　commentに紐づいたbook_id↓　　　　
+    # 　　　　　　　　　comment.id↓　　　commentに紐づいたbook_id↓
     @book_comment = BookComment.find_by(id: params[:id], book_id: params[:book_id]).destroy
     # 下記の記述でも問題ない。
     # book_comment = BookComment.find(params[:id])
     # book_comment.destroy
-    @book = Book.find(params[:book_id])
     # render :book_comments
     # redirect_back fallback_location: root_path　下の記述の方が良い？
     # redirect_to request.referer
